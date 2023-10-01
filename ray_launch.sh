ca678b44730a214682c9a5fe78ce608615596237
Nathan Cooper <nathanacoop@gmail.com>
2023-10-01 08:45:00 -0500

    Create ray_launch.sh

---
+++ ray_launch.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#SBATCH --partition=g40
+#SBATCH --job-name=applied_ablation
+#SBATCH --nodes 12
+#SBATCH --ntasks-per-node 1
+#SBATCH --exclusive
+#SBATCH --mem=999g
+#SBATCH --output=raylogs/%x_%j.out
+#SBATCH --account stablegpt
+
+srun --account stablegpt sh /admin/home-<user_name>/ray_worker.sh
