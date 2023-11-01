a83f5415bd59e3464c6856964769b477bb00b0d6
ncoop57 <nathan.cooper@stability.ai>
2023-11-01 01:21:51 +0000

    Add harry's version of code

---
+++ harry_embedding.py
@@ -0,0 +1,65 @@
+import fsspec
+from tqdm import tqdm
+import ray
+# import ray
+import os
+import pandas as pd
+import numpy as np
+from transformers import AutoModel
+import torch
+import time
+
+ray.init(address="auto", ignore_reinit_error=True) # , _temp_dir='/admin/home-nathan/semantic_doremi/ray_tmp/')
+# ray.init(include_dashboard=True, dashboard_host='0.0.0.0', dashboard_port=8265)
+
+fs = fsspec.filesystem("s3")
+file_list = fs.glob("s3://pile-everything-west/redpajama_raw/c4/*.jsonl")
+file_list = ['s3://' + string for string in file_list]
+model_name = "jinaai/jina-embeddings-v2-small-en"
+
+@ray.remote(num_gpus=1,num_cpus=6,max_retries=10)
+def worker(file_list):
+    model = AutoModel.from_pretrained(model_name, trust_remote_code=True)
+    model.eval().to('cuda')
+    model = torch.compile(model)
+    out_fs = fsspec.filesystem(
+        "s3",
+        config_kwargs={
+            "retries": {"max_attempts": 10},
+            "max_pool_connections": 512
+        }
+    )
+    for x in tqdm(file_list, desc="Files", position=0):
+        with out_fs.open(x, "rb") as f:
+            df = pd.read_json(f, lines=True)
+        batch_size = 8192 # 16384, 32768, 65536
+        n = len(df)
+        splits = n // batch_size
+        batches = np.array_split(df, splits)
+        all_embeddings = []
+        for batch in tqdm(batches, desc="Batches", position=1, leave=False):
+            try:
+                torch.cuda.empty_cache()
+                embeddings = model.encode(batch["text"].tolist(), max_length=1024)
+            except Exception as e:
+                print(e)
+                slurm_job_id = os.environ["SLURM_JOB_ID"]
+                # dump the batch to a txt file
+                with open(f"error_{slurm_job_id}.txt", "w") as f:
+                    f.write(str(batch['text'].tolist()))
+                raise ValueError(f"Error with batch at {x}")
+            all_embeddings.extend(embeddings)
+        df['embeddings'] = all_embeddings
+        with out_fs.open(x.replace("redpajama_raw","redpajama_processed"), "wb") as f:
+            df.to_parquet(f)
+        print("Done with file: " + x)
+
+file_list.sort()
+
+partitions = 8*1
+
+file_list = np.array_split(file_list, partitions)
+
+workers = [worker.remote(x) for x in file_list]
+
+ray.get(workers)
